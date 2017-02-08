cecef277e07f9b8d4fc7772e4a4936201ee5128b
Leandro Alves <leandrosustenido@gmail.com>
2017-02-08 21:13:19 -0200

    objectview

--- team.py
+++ team.py
@@ -1,42 +1,53 @@
 import http.client
 import json
 
+liverpool_id = 99
+
 connection = http.client.HTTPConnection('api.football-data.org')
 headers = { 'X-Auth-Token': '56a2a2ce412f41ebbbc076c0b025538b', 'X-Response-Control': 'minified' }
-connection.request('GET', '/v1/teams/64/fixtures', None, headers )
-
+connection.request('GET', '/v1/teams/' + str(liverpool_id) + '/fixtures', None, headers )
 
 raw_data = json.loads(connection.getresponse().read().decode())
 	
 fixtures = raw_data['fixtures']
 
+class objectview(object):
+    def __init__(self, d):
+        self.__dict__ = d
 
 # get actual round
 def get_actual_round():
 	for round in range(len(fixtures)):
-		if('TIMED' in fixtures[round].values()):
+		if(('TIMED' or 'LIVE') in fixtures[round].values()):
 			return round
 
 def get_next_match():
-	return fixtures[get_actual_round()]
+	actual_round = get_actual_round()
+	return objectview(fixtures[actual_round])
 
 def is_home():
-	if(get_next_match()['homeTeamId'] == 64):
+	next_match = get_next_match()
+	if(next_match.homeTeamId == liverpool_id):
 		return True
-	elif(get_next_match()['awayTeamId'] == 64):
+	elif(next_match.homeTeamId == liverpool_id):
 		return False
 	else:
 		print("Not a LFC match")
 
-
-def get_next_opponent_name():
-	if(is_home()):
-		return get_next_match()['awayTeamName']
-	else:
-		return get_next_match()['homeTeamName']
-
 def get_next_opponent_id():
+	next_match = get_next_match()
 	if(is_home()):
-		return get_next_match()['awayTeamId']
+		return next_match.awayTeamId
 	else:
-		return get_next_match()['homeTeamId']
+		return next_match.homeTeamId
+
+def create_team(team_id):
+	connection.request('GET', '/v1/teams/'+str(team_id), None, headers)
+	team = objectview(json.loads(connection.getresponse().read().decode()))
+	return team
+
+def create_liverpool():
+	return create_team(liverpool_id)
+
+def create_opponent():
+	return create_team(get_next_opponent_id())
